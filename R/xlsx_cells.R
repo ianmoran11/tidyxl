7fc4ad2622ac81a3cb45381065541d58109d4207
Ian Moran <ian.moran11@gmail.com>
2019-08-10 20:56:08 +1000

    fmt function

--- R/xlsx_cells.R
+++ R/xlsx_cells.R
@@ -153,23 +153,3 @@ xlsx_cells <- function(path, sheets = NA, check_filetype = TRUE,
               include_blank_cells)
 }
 
-#' Produce dataframe from excel file, storing formats as an attribute. 
-#'
-#' This function produces a data frame from excel file, storing formats as an attribute
-#' @param path path to excel file
-#' @param sheets sheet to be extracted.
-#'
-#' @export
-
-xlsx_cells_fmt <-  function(path, sheets = NA) {
-    sheets <- sheets
-    cells <- tidyxl::xlsx_cells(path, sheets = sheets)
-    
-    
-    cells <-
-    cells %>% select(address,row,col,data_type,character, numeric,date,logical,error, is_blank,local_format_id, everything(), sheet)
-    
-
-    attr(cells, "formats") <- tidyxl::xlsx_formats(path)
-    cells
-  }
